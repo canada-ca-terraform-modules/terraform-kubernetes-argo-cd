86ed1ecdf46f285a7251a091cd42ae6bc355094b
Adam Glavine <adam.glavine@cloud.statcan.ca>
2023-02-02 20:17:12 +0000

    chore: add sensitive flag to Terraform variables, where applicable

--- variables.tf
+++ variables.tf
@@ -7,15 +7,24 @@
 variable "helm_namespace" {}
 
 variable "helm_repository" {
-  default = "https://statcan.github.io/charts"
+  default     = "https://statcan.github.io/charts"
+  description = "The repository where the Helm chart is stored"
 }
 
 variable "helm_repository_password" {
-  default = ""
+  type        = string
+  nullable    = false
+  default     = ""
+  description = "The password of the repository where the Helm chart is stored"
+  sensitive   = true
 }
 
 variable "helm_repository_username" {
-  default = ""
+  type        = string
+  nullable    = false
+  default     = ""
+  description = "The username of the repository where the Helm chart is stored"
+  sensitive   = true
 }
 
 variable "helm_chart_version" {
